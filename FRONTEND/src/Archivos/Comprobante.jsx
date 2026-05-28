2837270a86f82e8a7a111963130e84108e4dff91
Luisa Castillo <dkim44243@gmail.com>
2026-05-27 22:51:34 -0500

    modificacion de archivo

--- FRONTEND/src/Archivos/Comprobante.jsx
+++ FRONTEND/src/Archivos/Comprobante.jsx
@@ -22,6 +22,7 @@ const Comprobante = () => {
       });
 
       alert("Factura enviada al correo del cliente con éxito!");
+      console.log(res.data)
     } catch (error) {
       console.error(error);
       alert("El archivo aún no está en la carpeta o el nombre no coincide (Debe ser factura_ID.pdf)");
