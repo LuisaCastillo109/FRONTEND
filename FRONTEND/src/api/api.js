110219f22a4e26c23d8449f36f8a7fa6c4103d79
LuisaCastillo <163435574+LuisaCastillo109@users.noreply.github.com>
2026-08-18 19:35:59 -0500

    Update api.js

--- FRONTEND/src/api/api.js
+++ FRONTEND/src/api/api.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
+const usuario = JSON.parse(localStorage.getItem("usuario"));
+const Token = localStorage.getItem("Token");
 
-  const usuario = JSON.parse(localStorage.getItem("usuario"));
-  const Token = localStorage.getItem("Token");
-  const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-  headers: {
-  Authorization: `Bearer ${Token}`
-  }});
+console.log("🔥 API URL:", process.env.REACT_APP_API_URL);
+
+const api = axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
+    headers: {
+        Authorization: `Bearer ${Token}`
+    }
+});
 
 export default api;
